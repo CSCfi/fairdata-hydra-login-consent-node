8af1a00a1f8d6b2126d039338417f59efe9e89e8
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-12-09 14:47:19 +0200

    Test the dev config.

--- routes/login.js
+++ routes/login.js
@@ -52,7 +52,7 @@ router.post('/', csrfProtection, function (req, res, next) {
 
   // Let's check if the user provided valid credentials. Of course, you'd use a database or some third-party service
   // for this!
-  if (!(req.body.email === 'foo@bar.com' && req.body.password === 'foobar')) {
+  if (!(req.body.username === 'teppo' && req.body.password === 'testaaja')) {
     // Looks like the user provided invalid credentials, let's show the ui again...
 
     res.render('login', {
@@ -68,7 +68,7 @@ router.post('/', csrfProtection, function (req, res, next) {
   // Seems like the user authenticated! Let's tell hydra...
   hydra.acceptLoginRequest(challenge, {
     // Subject is an alias for user ID. A subject can be a random string, a UUID, an email address, ....
-    subject: 'foo@bar.com',
+    subject: 'teppo@fairdataid',
 
     // This tells hydra to remember the browser and automatically authenticate the user in future requests. This will
     // set the "skip" parameter in the other route to true on subsequent requests!
